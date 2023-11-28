949a1d522833d5bbf1aead53138c30efb020fc44
Oscar Devora <oscar_devora@yahoo.com>
2023-11-28 15:24:19 -0600

    fix sg name

--- security_group/main.tf
+++ security_group/main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "ec2" {
     ami = "ami-0c55b159cbfafe1f0"
     instance_type = "t2.micro"
-    security_groups = [aws.inboundTraffic.name, aws_security_group.outboundTraffic.name]
+    security_groups = [aws_security_group.inboundTraffic.name, aws_security_group.outboundTraffic.name]
       tags = {
         Name = "terraform-example"
       }
